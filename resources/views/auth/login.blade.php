df8c31b91f40fd3e1ddd844ca561aac7f1a538b4
dedektegarr <dedektegarr@gmail.com>
2023-05-18 20:59:37 +0700

    menambah validasi saat login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,12 +11,16 @@
                     <form action="{{ route('login.store') }}" method="post">
                         @csrf
                         <div class="input-group mb-3">
-                            <input type="email" class="form-control" placeholder="Email" name="email">
+                            <input type="email" class="form-control @error('email') is-invalid @enderror"
+                                placeholder="Email" name="email">
                             <div class="input-group-append">
                                 <div class="input-group-text">
                                     <span class="fas fa-envelope"></span>
                                 </div>
                             </div>
+                            @error('email')
+                                <span class="invalid-feedback">{{ $message }}</span>
+                            @enderror
                         </div>
                         <div class="input-group mb-3">
                             <input type="password" class="form-control" placeholder="Password" name="password">
